a8bc1c20618f157ca796dd95cb51dbd6acb69415
DESKTOP-58EJ12O\Hafiz Naeem Hassan <naeem.alpha2labs1@gmail.com>
2019-05-06 15:26:50 +0500

    Fetch Contacts from Mobile and Show in our App

---
+++ app/src/main/java/com/example/freshfinaltoolbarwhatsapp/TestUIActivity.java
@@ -0,0 +1,35 @@
+package com.example.freshfinaltoolbarwhatsapp;
+
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class TestUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_ui);
+        checkPermissions();
+        getContactList();
+    }
+
+    private void getContactList() {
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
+        while (phones.moveToNext()) {
+            String Name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            number = number.replaceAll("\\s+", "");
+            Toast.makeText(TestUIActivity.this, Name + "\n" + number, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            requestPermissions(new String[]{"android.permission.READ_CONTACTS"}, 1);
+    }
+}
